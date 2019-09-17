2ad72564bfa7b9313ccbc63a362e4901f69ed46d
ErdbeerbaerLP <27149563+ErdbeerbaerLP@users.noreply.github.com>
2019-09-17 17:43:43 +0200

    Bugfix

--- src/main/java/codes/biscuit/skyblockaddons/mixins/MixinGuiInventory.java
+++ src/main/java/codes/biscuit/skyblockaddons/mixins/MixinGuiInventory.java
@@ -7,6 +7,7 @@ import net.minecraft.client.gui.inventory.GuiInventory;
 import net.minecraft.inventory.ClickType;
 import net.minecraft.inventory.Container;
 import net.minecraft.inventory.Slot;
+import net.minecraft.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 
@@ -34,7 +35,7 @@ public abstract class MixinGuiInventory extends GuiContainer {
         int k = guiTop;
         boolean isOutsideGui = oldMouseX < j || oldMouseY < k || oldMouseX >= j + xSize || oldMouseY >= k + ySize;
         if (main.getConfigValues().isEnabled(Feature.STOP_DROPPING_SELLING_RARE_ITEMS) &&
-                mc.player.inventory.getItemStack() != null && isOutsideGui &&
+                mc.player.inventory.getItemStack() != ItemStack.EMPTY && isOutsideGui &&
                 main.getInventoryUtils().shouldCancelDrop(mc.player.inventory.getItemStack())) return;
         super.handleMouseClick(slotIn, slotId, clickedButton, clickType);
     }
