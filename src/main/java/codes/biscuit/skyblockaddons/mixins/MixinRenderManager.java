40dff8580ddb32696d3535c457bee2b4b779c5ae
Biscuit <jlrochelle45@gmail.com>
2019-09-16 23:03:53 -0400

    Fixed common crash. Updated version.

--- src/main/java/codes/biscuit/skyblockaddons/mixins/MixinRenderManager.java
+++ src/main/java/codes/biscuit/skyblockaddons/mixins/MixinRenderManager.java
@@ -37,7 +37,8 @@ public class MixinRenderManager {
             }
             if (main.getConfigValues().isEnabled(Feature.HIDE_PLAYERS_IN_LOBBY) &&
                     (entityIn instanceof EntityOtherPlayerMP || entityIn instanceof EntityFX || entityIn instanceof EntityItemFrame) &&
-                    entityIn.getDistanceToEntity(Minecraft.getMinecraft().thePlayer) > 7 && main.getUtils().getLocation().equals(EnumUtils.Location.VILLAGE)) {
+                    entityIn.getDistanceToEntity(Minecraft.getMinecraft().thePlayer) > 7 && main.getUtils().getLocation() != null &&
+                    main.getUtils().getLocation().equals(EnumUtils.Location.VILLAGE)) {
                 cir.setReturnValue(false);
             }
         }
