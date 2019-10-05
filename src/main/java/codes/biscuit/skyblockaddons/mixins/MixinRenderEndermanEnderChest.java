6965d4d342499385551da034d3ccc4e336dce8ff
Brian Graham <support@netcoding.net>
2019-10-05 01:28:46 -0400

    Forgot this parameter

--- src/main/java/codes/biscuit/skyblockaddons/mixins/MixinRenderEndermanEnderChest.java
+++ src/main/java/codes/biscuit/skyblockaddons/mixins/MixinRenderEndermanEnderChest.java
@@ -12,6 +12,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(RenderEnderman.class)
 public abstract class MixinRenderEndermanEnderChest {
@@ -25,7 +26,7 @@ public abstract class MixinRenderEndermanEnderChest {
 					value = "HEAD"
 			)
 	)
-	private void getEntityTexture(EntityEnderman entity) {
+	private void getEntityTexture(EntityEnderman entity, CallbackInfoReturnable cir) {
 		SkyblockAddons main = SkyblockAddons.getInstance();
 
 		if (main.getUtils().isOnSkyblock()) {
