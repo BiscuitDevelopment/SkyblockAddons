521f191ce07d2ab8ddb2e530cd9980158bf018eb
DidiSkywalker <corvin.eberlein@gmail.com>
2020-02-27 20:09:15 +0100

    Fixed jungle axe cooldown - Changed to effective cooldown of 1.5s - Only goes in cooldown when breaking logs - Other items don't go into cooldown when breaking blocks
    Removed logs from API requests when the Magma Timer feature is disabled

--- src/main/java/codes/biscuit/skyblockaddons/asm/PlayerControllerMPTransformer.java
+++ src/main/java/codes/biscuit/skyblockaddons/asm/PlayerControllerMPTransformer.java
@@ -36,10 +36,13 @@ public class PlayerControllerMPTransformer implements ITransformer {
 
                 // Objective:
                 // Find: Method head.
-                // Insert: PlayerControllerMPHook.onPlayerDestroyBlock();
+                // Insert:   ReturnValue returnValue = new ReturnValue();
+                //           PlayerControllerMPHook.onPlayerDestroyBlock(loc, returnValue);
+                //           if (returnValue.isCancelled()) {
+                //               return false;
+                //           }
 
-                methodNode.instructions.insertBefore(methodNode.instructions.getFirst(), new MethodInsnNode(Opcodes.INVOKESTATIC,
-                        "codes/biscuit/skyblockaddons/asm/hooks/PlayerControllerMPHook", "onPlayerDestroyBlock", "()V", false));
+                methodNode.instructions.insertBefore(methodNode.instructions.getFirst(), insertOnDestroyBlock());
             } else if (TransformerMethod.windowClick.matches(methodNode)) {
 
                 // Objective:
@@ -66,6 +69,32 @@ public class PlayerControllerMPTransformer implements ITransformer {
         list.add(new VarInsnNode(Opcodes.ALOAD, 1)); // loc
         list.add(new VarInsnNode(Opcodes.ALOAD, 5)); // PlayerControllerMPHook.onPlayerDamageBlock(loc, returnValue);
         list.add(new MethodInsnNode(Opcodes.INVOKESTATIC, "codes/biscuit/skyblockaddons/asm/hooks/PlayerControllerMPHook", "onPlayerDamageBlock",
+                "("+ TransformerClass.BlockPos.getName()+"Lcodes/biscuit/skyblockaddons/asm/utils/ReturnValue;)V", false));
+
+        list.add(new VarInsnNode(Opcodes.ALOAD, 5));
+        list.add(new MethodInsnNode(Opcodes.INVOKEVIRTUAL, "codes/biscuit/skyblockaddons/asm/utils/ReturnValue", "isCancelled",
+                "()Z", false));
+        LabelNode notCancelled = new LabelNode(); // if (returnValue.isCancelled())
+        list.add(new JumpInsnNode(Opcodes.IFEQ, notCancelled));
+
+        list.add(new InsnNode(Opcodes.ICONST_0)); // return false;
+        list.add(new InsnNode(Opcodes.IRETURN));
+        list.add(notCancelled);
+
+        return list;
+    }
+
+    private InsnList insertOnDestroyBlock() {
+        InsnList list = new InsnList();
+
+        list.add(new TypeInsnNode(Opcodes.NEW, "codes/biscuit/skyblockaddons/asm/utils/ReturnValue"));
+        list.add(new InsnNode(Opcodes.DUP)); // ReturnValue returnValue = new ReturnValue();
+        list.add(new MethodInsnNode(Opcodes.INVOKESPECIAL, "codes/biscuit/skyblockaddons/asm/utils/ReturnValue", "<init>", "()V", false));
+        list.add(new VarInsnNode(Opcodes.ASTORE, 5));
+
+        list.add(new VarInsnNode(Opcodes.ALOAD, 1)); // loc
+        list.add(new VarInsnNode(Opcodes.ALOAD, 5)); // PlayerControllerMPHook.onPlayerDestroyBlock(loc);
+        list.add(new MethodInsnNode(Opcodes.INVOKESTATIC, "codes/biscuit/skyblockaddons/asm/hooks/PlayerControllerMPHook", "onPlayerDestroyBlock",
                 "("+ TransformerClass.BlockPos.getName()+"Lcodes/biscuit/skyblockaddons/asm/utils/ReturnValue;)V", false));
 
         list.add(new VarInsnNode(Opcodes.ALOAD, 5));
