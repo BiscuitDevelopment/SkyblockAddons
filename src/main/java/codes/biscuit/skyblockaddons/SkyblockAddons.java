e1e8f2c72df4ec928824910db7d9543b05365784
biscuut <jlrochelle45@gmail.com>
2020-03-18 01:02:56 -0400

    Update build

--- src/main/java/codes/biscuit/skyblockaddons/SkyblockAddons.java
+++ src/main/java/codes/biscuit/skyblockaddons/SkyblockAddons.java
@@ -34,7 +34,7 @@ public class SkyblockAddons {
 
     static final String MOD_ID = "skyblockaddons";
     public static final String MOD_NAME = "SkyblockAddons";
-    public static final String VERSION = "1.5.0-b8";
+    public static final String VERSION = "1.5.0-b9";
 
     /** The main instance of the mod, used mainly my mixins who don't get it passed to them. */
     @Getter private static SkyblockAddons instance;
