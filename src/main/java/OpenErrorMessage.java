835fafaaf6d7fbf5bee533b34a213f710bc24251
ILikePlayingGames <22475143+ILikePlayingGames@users.noreply.github.com>
2020-03-27 20:01:59 -0400

    Add link to new installation instructions in OpenErrorMessage

--- src/main/java/OpenErrorMessage.java
+++ src/main/java/OpenErrorMessage.java
@@ -1,4 +1,8 @@
 import javax.swing.JOptionPane;
+import java.awt.*;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 /**
  * Shows an error message if the mod jar is opened directly instead of in forge.
@@ -6,11 +10,18 @@ import javax.swing.JOptionPane;
 public class OpenErrorMessage {
 
     public static void main(String[] args) {
-        JOptionPane.showMessageDialog(null,
-                "Opening this file directly does nothing, please install forge and put this file in your .minecraft/mods folder. " +
-                        System.lineSeparator() + "If you are unsure how, you can search for \"Forge Installation Tutorials\" online, or get help by joining my discord: biscuit.codes/discord.",
-                "You are installing this incorrectly!",
-                JOptionPane.INFORMATION_MESSAGE);
+        String[] options = {"Ok", "Help"};
+        int response = JOptionPane.showOptionDialog(null, "Opening this file directly does nothing, please install Forge and put this file in your .minecraft/mods folder. " + System.lineSeparator() +
+                "If you are unsure how, please click \"Help.\"", "You are installing this incorrectly!", JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null, options, null);
+
+        if (response == 1) {
+            try {
+                Desktop.getDesktop().browse(new URI("https://github.com/BiscuitDevelopment/SkyblockAddons/wiki/FAQ#how-do-i-install-skyblockaddons"));
+            } catch (IOException | URISyntaxException e) {
+                e.printStackTrace();
+            }
+        }
+
         System.exit(0);
     }
 }
