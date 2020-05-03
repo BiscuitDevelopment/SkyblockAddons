b059651b48f3a5e5b78525be188458f379f2b2e0
DJtheRedstoner <jarabekit@gmail.com>
2020-05-03 09:36:36 -0400

    Remove non-legendary sea creatures used for debugging

--- src/main/java/codes/biscuit/skyblockaddons/listeners/PlayerListener.java
+++ src/main/java/codes/biscuit/skyblockaddons/listeners/PlayerListener.java
@@ -54,7 +54,7 @@ public class PlayerListener {
     private final Pattern SWITCH_PROFILE_CHAT_PATTERN = Pattern.compile("§aYour profile was changed to: §e([A-Za-z]+).*");
     private final Pattern COLLECTIONS_CHAT_PATTERN = Pattern.compile("§.\\+(?:§[0-9a-f])?([0-9.]+) §?[0-9a-f]?([A-Za-z]+) (\\([0-9.,]+/[0-9.,]+\\))");
     private final Set<String> randomMessages = new HashSet<>(Arrays.asList("I feel like I can fly!", "What was in that soup?", "Hmm… tasty!", "Hmm... tasty!", "You can now fly for 2 minutes.", "Your Magical Mushroom Soup flight has been extended for 2 extra minutes."));
-    private final Set<String> legendarySeaCreatureMessages = new HashSet<>(Arrays.asList("You caught a lowly Squid.", "The Water Hydra has come to test your strength.", "The Sea Emperor arises from the depths...", "What is this creature!?"));
+    private final Set<String> legendarySeaCreatureMessages = new HashSet<>(Arrays.asList("The Water Hydra has come to test your strength.", "The Sea Emperor arises from the depths...", "What is this creature!?"));
 
 
     private boolean sentUpdate = false;
