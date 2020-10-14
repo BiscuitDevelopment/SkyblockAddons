cae3214cd00c2b85ceeaaa894f6b8f7ad749c101
RobotHanzo <36107150+RobotHanzo@users.noreply.github.com>
2020-10-14 16:39:55 +0800

    Fix crashes caused by duplicated feature IDs
    Fixed by changing the IDs

--- src/main/java/codes/biscuit/skyblockaddons/core/Feature.java
+++ src/main/java/codes/biscuit/skyblockaddons/core/Feature.java
@@ -180,8 +180,8 @@ public enum Feature {
     DISABLE_BOSS_MESSAGES(148, "settings.disableBossMessages", null, false),
     SHOW_SWORD_KILLS(149, "settings.showSwordKills", new GuiFeatureData(ColorCode.RED, true), false),
 
-    HIDE_OTHER_PLAYERS_PRESENTS(141, "settings.hideOtherPlayersPresents", null,false),
-    EASIER_PRESENT_OPENING(142, "settings.easierPresentOpening", null,false),
+    HIDE_OTHER_PLAYERS_PRESENTS(150, "settings.hideOtherPlayersPresents", null,false),
+    EASIER_PRESENT_OPENING(151, "settings.easierPresentOpening", null,false),
 
     WARNING_TIME(-1, Message.SETTING_WARNING_DURATION, false),
 
