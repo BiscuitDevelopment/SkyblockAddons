f9eed86169703f7217555757db66d214d5e25871
Phoube <11449309+Phoube@users.noreply.github.com>
2021-02-05 01:05:42 -0500

    Fixed an ASM glitch in MinecraftHook that would cause the "Feature prevented breaking..." message to appear when the player wasn't holding down the break button

--- src/main/java/codes/biscuit/skyblockaddons/asm/MinecraftTransformer.java
+++ src/main/java/codes/biscuit/skyblockaddons/asm/MinecraftTransformer.java
@@ -76,9 +76,11 @@ public class MinecraftTransformer implements ITransformer {
                     }
                 }
             }
-            if (TransformerMethod.clickMouse.matches(methodNode) || TransformerMethod.sendClickBlockToController.matches(methodNode)) {
-
+            if (TransformerMethod.clickMouse.matches(methodNode)) {
                 methodNode.instructions.insertBefore(methodNode.instructions.getFirst(), insertOnClickMouse());
+            }
+            if (TransformerMethod.sendClickBlockToController.matches(methodNode)) {
+                methodNode.instructions.insertBefore(methodNode.instructions.getFirst(), insertOnSendClickBlockToController());
             }
         }
     }
@@ -131,6 +133,32 @@ public class MinecraftTransformer implements ITransformer {
         list.add(new VarInsnNode(Opcodes.ALOAD, 3)); // MinecraftHook.onClickMouse(ReturnValue);
         list.add(new MethodInsnNode(Opcodes.INVOKESTATIC, "codes/biscuit/skyblockaddons/asm/hooks/MinecraftHook", "onClickMouse",
                 "(Lcodes/biscuit/skyblockaddons/asm/utils/ReturnValue;)V", false));
+
+        list.add(new VarInsnNode(Opcodes.ALOAD, 3));
+        list.add(new MethodInsnNode(Opcodes.INVOKEVIRTUAL, "codes/biscuit/skyblockaddons/asm/utils/ReturnValue", "isCancelled", "()Z", false));
+        LabelNode notCancelled = new LabelNode(); // if (returnValue.isCancelled())
+        list.add(new JumpInsnNode(Opcodes.IFEQ, notCancelled));
+
+        list.add(new InsnNode(Opcodes.RETURN)); // return;
+        list.add(notCancelled);
+
+        return list;
+    }
+
+
+
+    private InsnList insertOnSendClickBlockToController() {
+        InsnList list = new InsnList();
+
+        list.add(new TypeInsnNode(Opcodes.NEW, "codes/biscuit/skyblockaddons/asm/utils/ReturnValue"));
+        list.add(new InsnNode(Opcodes.DUP)); // ReturnValue returnValue = new ReturnValue();
+        list.add(new MethodInsnNode(Opcodes.INVOKESPECIAL, "codes/biscuit/skyblockaddons/asm/utils/ReturnValue", "<init>", "()V", false));
+        list.add(new VarInsnNode(Opcodes.ASTORE, 3));
+
+        list.add(new VarInsnNode(Opcodes.ILOAD, 1));
+        list.add(new VarInsnNode(Opcodes.ALOAD, 3)); // MinecraftHook.onSendClickBlockToController(leftClick, ReturnValue);
+        list.add(new MethodInsnNode(Opcodes.INVOKESTATIC, "codes/biscuit/skyblockaddons/asm/hooks/MinecraftHook", "onSendClickBlockToController",
+                "(ZLcodes/biscuit/skyblockaddons/asm/utils/ReturnValue;)V", false));
 
         list.add(new VarInsnNode(Opcodes.ALOAD, 3));
         list.add(new MethodInsnNode(Opcodes.INVOKEVIRTUAL, "codes/biscuit/skyblockaddons/asm/utils/ReturnValue", "isCancelled", "()Z", false));
--- src/main/java/codes/biscuit/skyblockaddons/asm/hooks/MinecraftHook.java
+++ src/main/java/codes/biscuit/skyblockaddons/asm/hooks/MinecraftHook.java
@@ -11,6 +11,7 @@ import codes.biscuit.skyblockaddons.utils.ItemUtils;
 import lombok.Getter;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.entity.EntityPlayerSP;
 import net.minecraft.client.resources.IReloadableResourceManager;
@@ -41,10 +42,23 @@ public class MinecraftHook {
     private static final Set<Location> DEEP_CAVERNS_LOCATIONS = EnumSet.of(Location.DEEP_CAVERNS, Location.GUNPOWDER_MINES,
             Location.LAPIS_QUARRY, Location.PIGMAN_DEN, Location.SLIMEHILL, Location.DIAMOND_RESERVE, Location.OBSIDIAN_SANCTUARY);
 
+    private static final Set<Location> DWARVEN_MINES_LOCATIONS = EnumSet.of(Location.DWARVEN_MINES, Location.THE_LIFT, Location.DWARVEN_VILLAGE,
+            Location.GATES_TO_THE_MINES, Location.THE_FORGE, Location.FORGE_BASIN, Location.LAVA_SPRINGS, Location.PALACE_BRIDGE,
+            Location.ROYAL_PALACE, Location.ARISTOCRAT_PASSAGE, Location.HANGING_TERRACE, Location.CLIFFSIDE_VEINS,
+            Location.RAMPARTS_QUARRY, Location.DIVANS_GATEWAY, Location.FAR_RESERVE, Location.GOBLIN_BURROWs, Location.UPPER_MINES,
+            Location.MINERS_GUILD, Location.GREAT_ICE_WALL, Location.THE_MIST, Location.CC_MINECARTS_CO, Location.GRAND_LIBRARY,
+            Location.HANGING_COURT);
+
     private static final Set<Block> DEEP_CAVERNS_MINEABLE_BLOCKS = new HashSet<>(Arrays.asList(Blocks.coal_ore, Blocks.iron_ore, Blocks.gold_ore, Blocks.redstone_ore, Blocks.emerald_ore,
             Blocks.diamond_ore, Blocks.diamond_block, Blocks.obsidian, Blocks.lapis_ore, Blocks.lit_redstone_ore));
 
     private static final Set<Block> NETHER_MINEABLE_BLOCKS = new HashSet<>(Arrays.asList(Blocks.glowstone, Blocks.quartz_ore, Blocks.nether_wart));
+
+    // TODO: Make this less computationally expensive
+    // More specifically, should be cyan hardened clay, grey/light blue wool, dark prismarine, prismarine brick, prismarine, polished diorite
+    private static final Set<String> DWARVEN_MINEABLE_BLOCKS = new HashSet<>(Arrays.asList("minecraft:prismarine0",
+            "minecraft:prismarine1", "minecraft:prismarine2", "minecraft:stone4", "minecraft:wool3", "minecraft:wool7",
+            "minecraft:stained_hardened_clay9"));
 
     private static final Set<Location> PARK_LOCATIONS = EnumSet.of(Location.BIRCH_PARK, Location.SPRUCE_WOODS, Location.SAVANNA_WOODLAND, Location.DARK_THICKET, Location.JUNGLE_ISLAND);
 
@@ -123,6 +137,7 @@ public class MinecraftHook {
     }
 
     public static void onClickMouse(ReturnValue<?> returnValue) {
+
         Minecraft mc = Minecraft.getMinecraft();
         if (mc.objectMouseOver == null || mc.objectMouseOver.typeOfHit != MovingObjectPosition.MovingObjectType.BLOCK) {
             return;
@@ -135,8 +150,11 @@ public class MinecraftHook {
         SkyblockAddons main = SkyblockAddons.getInstance();
         EntityPlayerSP p = mc.thePlayer;
         ItemStack heldItem = p.getHeldItem();
-        if (heldItem != null) {
-            Block block = mc.theWorld.getBlockState(blockPos).getBlock();
+        if (heldItem != null && main.getUtils().isOnSkyblock()) {
+            // TODO: tmp fix for Dwarven mines. Problem is that block doesn't contain metadata
+            IBlockState state =  mc.theWorld.getBlockState(blockPos);
+            Block block = state.getBlock();
+            String id = "" + Block.blockRegistry.getNameForObject(block) + block.getMetaFromState(state);
             long now = System.currentTimeMillis();
 
             if (main.getConfigValues().isEnabled(Feature.AVOID_BREAKING_STEMS) && (block.equals(Blocks.melon_stem) || block.equals(Blocks.pumpkin_stem))) {
@@ -146,14 +164,14 @@ public class MinecraftHook {
                 }
                 returnValue.cancel();
             } else if (main.getConfigValues().isEnabled(Feature.ONLY_MINE_ORES_DEEP_CAVERNS) && DEEP_CAVERNS_LOCATIONS.contains(main.getUtils().getLocation())
-                    && ItemUtils.isPickaxe(heldItem.getItem()) && !DEEP_CAVERNS_MINEABLE_BLOCKS.contains(block)) {
+                    && ItemUtils.isPickaxe(heldItem) && !DEEP_CAVERNS_MINEABLE_BLOCKS.contains(block)) {
                 if (main.getConfigValues().isEnabled(Feature.ENABLE_MESSAGE_WHEN_MINING_DEEP_CAVERNS) && now - lastUnmineableMessage > 60000) {
                     lastUnmineableMessage = now;
                     main.getUtils().sendMessage(main.getConfigValues().getRestrictedColor(Feature.ONLY_MINE_ORES_DEEP_CAVERNS) + Message.MESSAGE_CANCELLED_NON_ORES_BREAK.getMessage());
                 }
                 returnValue.cancel();
             } else if (main.getConfigValues().isEnabled(Feature.ONLY_MINE_VALUABLES_NETHER) && Location.BLAZING_FORTRESS.equals(main.getUtils().getLocation()) &&
-                    ItemUtils.isPickaxe(heldItem.getItem()) && !NETHER_MINEABLE_BLOCKS.contains(block)) {
+                    ItemUtils.isPickaxe(heldItem) && !NETHER_MINEABLE_BLOCKS.contains(block)) {
                 if (main.getConfigValues().isEnabled(Feature.ENABLE_MESSAGE_WHEN_MINING_NETHER) && now - lastUnmineableMessage > 60000) {
                     lastUnmineableMessage = now;
                     main.getUtils().sendMessage(main.getConfigValues().getRestrictedColor(Feature.ONLY_MINE_VALUABLES_NETHER) + Message.MESSAGE_CANCELLED_NON_ORES_BREAK.getMessage());
@@ -164,6 +182,13 @@ public class MinecraftHook {
                 if (main.getConfigValues().isEnabled(Feature.ENABLE_MESSAGE_WHEN_BREAKING_PARK) && now - lastUnmineableMessage > 60000) {
                     lastUnmineableMessage = now;
                     main.getUtils().sendMessage(main.getConfigValues().getRestrictedColor(Feature.ONLY_BREAK_LOGS_PARK) + Message.MESSAGE_CANCELLED_NON_LOGS_BREAK.getMessage());
+                }
+                returnValue.cancel();
+            } else if (main.getConfigValues().isEnabled(Feature.ONLY_MINE_ORES_DWARVEN_MINES) && DWARVEN_MINES_LOCATIONS.contains(main.getUtils().getLocation())
+                    && ItemUtils.isPickaxe(heldItem) && (!DWARVEN_MINEABLE_BLOCKS.contains(id) && !DEEP_CAVERNS_MINEABLE_BLOCKS.contains(block))) {
+                if (main.getConfigValues().isEnabled(Feature.ENABLE_MESSAGE_WHEN_BREAKING_PARK) && now - lastUnmineableMessage > 60000) {
+                    lastUnmineableMessage = now;
+                    main.getUtils().sendMessage(main.getConfigValues().getRestrictedColor(Feature.ONLY_MINE_ORES_DWARVEN_MINES) + Message.MESSAGE_CANCELLED_NON_ORES_BREAK.getMessage());
                 }
                 returnValue.cancel();
             } else if (main.getConfigValues().isEnabled(Feature.JUNGLE_AXE_COOLDOWN)) {
@@ -179,5 +204,13 @@ public class MinecraftHook {
                 }
             }
         }
+    }
+
+    public static void onSendClickBlockToController(boolean leftClick, ReturnValue<?> returnValue) {
+        // If we aren't trying to break anything, don't change vanilla behavior (was causing false positive chat messages)
+        if (!leftClick) {
+            return;
+        }
+        onClickMouse(returnValue);
     }
 }
