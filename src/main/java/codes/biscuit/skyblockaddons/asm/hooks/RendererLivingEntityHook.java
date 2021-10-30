24c20a8096d5d82a397885d1277d685e1610f7a9
nopothegamer <40329022+nopothegamer@users.noreply.github.com>
2021-10-30 18:15:23 +1100

    Update RendererLivingEntityHook.java

--- src/main/java/codes/biscuit/skyblockaddons/asm/hooks/RendererLivingEntityHook.java
+++ src/main/java/codes/biscuit/skyblockaddons/asm/hooks/RendererLivingEntityHook.java
@@ -16,7 +16,7 @@ import java.util.Set;
 public class RendererLivingEntityHook {
 
     // no don't ask to be added lol, for now these are just like my admins
-    private static final Set<String> coolPeople = Sets.newHashSet("Dinnerbone", "Biscut", "Pinpointed", "Berded", "Potat_owo", "Pnda__", "Throwpo");
+    private static final Set<String> coolPeople = Sets.newHashSet("Dinnerbone", "Biscut", "Pinpointed", "Berded", "Potat_owo", "Pnda__", "Throwpo", "StopUsingSBE");
     private static boolean isCoolPerson;
 
     public static boolean equals(String string, Object otherString) {
