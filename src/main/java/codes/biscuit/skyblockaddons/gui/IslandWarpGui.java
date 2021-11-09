87eb69a53e6ab5602c23eba62d43ca72212dfe97
nopothegamer <40329022+nopothegamer@users.noreply.github.com>
2021-11-09 18:55:32 +1100

    Change location of warp mines and forge
    So it fits better

--- src/main/java/codes/biscuit/skyblockaddons/gui/IslandWarpGui.java
+++ src/main/java/codes/biscuit/skyblockaddons/gui/IslandWarpGui.java
@@ -391,8 +391,8 @@ public class IslandWarpGui extends GuiScreen {
 
         GOLD_MINE("gold", Translations.getMessage("warpMenu.spawn"), Island.GOLD_MINE, true, 86, 259),
         DEEP_CAVERNS("deep", Translations.getMessage("warpMenu.spawn"), Island.DEEP_CAVERNS, true, 97, 213),
-        DWARVEN_MINES("mines", "Dwarven Mines", Island.DEEP_CAVERNS, false, 110, 280),
-        DWARVEN_FORGE("forge", "Forge", Island.DEEP_CAVERNS, true, 170, 310),
+        DWARVEN_MINES("mines", "Dwarven Mines", Island.DEEP_CAVERNS, false, 280, 205),
+        DWARVEN_FORGE("forge", "Forge", Island.DEEP_CAVERNS, true, 260, 280),
         CRYSTAL_HOLLOWS("crystals", "Crystal Hollows", Island.DEEP_CAVERNS, true, 220, 350),
 
         DUNGEON_HUB_ISLAND("dungeon_hub", Translations.getMessage("warpMenu.spawn"), Island.DUNGEON_HUB, false, 35, 80),
